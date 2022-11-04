08f5c902064295ba4e519c62f6efd25543c6c814
malteish <malte@corpus.ventures>
2022-11-04 15:06:42 +0100

    disable uneccessary extra checks

--- contracts/DeterministicDeployFactory.sol
+++ contracts/DeterministicDeployFactory.sol
@@ -22,18 +22,18 @@ contract DeterministicDeployFactory {
     function deploy(bytes32 _salt, address payable buyer, address payable _receiver, uint _amount, uint _tokenPrice, uint _expiration, IERC20 _currency, MintableERC20 _token) external {
         
         // calculate expected address
-        address expectedAddress = getAddress(_salt, buyer, _receiver, _amount, _tokenPrice, _expiration, _currency, _token);
+        //address expectedAddress = getAddress(_salt, buyer, _receiver, _amount, _tokenPrice, _expiration, _currency, _token);
         
         // for syntax, see: https://solidity-by-example.org/app/create2/
         address actualAddress = address(new PrivateInvite{salt: _salt}(buyer, _receiver, _amount, _tokenPrice, _expiration, _currency, _token));
         
-        // make sure some code has been uploaded to the address
-        uint len;
-        assembly { len := extcodesize(actualAddress) }
-        require(len != 0);
+        // // make sure some code has been uploaded to the address
+        // uint len;
+        // assembly { len := extcodesize(actualAddress) }
+        // require(len != 0);
 
-        // make sure actual address matches expected address
-        require(actualAddress == expectedAddress, "Actual address does not match expected address");
+        // // make sure actual address matches expected address
+        // require(actualAddress == expectedAddress, "Actual address does not match expected address");
         
         emit Deploy(actualAddress);
         //return addr;
