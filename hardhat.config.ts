b68a289c84d6205ecd26b8616a9e99f08c616049
Christoph Jentzsch <jentzsch.software@gmail.com>
2022-11-11 11:24:54 +0100

    make Token.sol compatible with ERC2771 and 2612 (permit)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
  const config: HardhatUserConfig =  {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
     localhost: {
       url: 'http://localhost:8545'
