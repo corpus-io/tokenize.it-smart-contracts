f1a104fbc83c90b33c9a35378b86e7b754c0e442
malteish <malteish@corpus.io>
2022-12-07 16:36:58 +0100

    actually remove setMintingAllowance

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -196,25 +196,6 @@ contract Token is ERC2771Context, ERC20Permit, Pausable, AccessControl {
     }
 
     /** 
-        @notice minting contracts such as personal investment invite, vesting, crowdfunding must be granted a minting allowance. 
-            Each call of setMintingAllowance will make the contract: 
-                1. forget how many tokens might have been minted by this minter before. 
-                2. set the allowance for this minter to the new value, discarding any remaining allowance that might have been left from before.
-            This feels very natural on the first call, but might be surprising on subsequent calls, so be careful. Before setting it to a non-zero value, it must be zero.
-        @dev The "forget last allowance and count of minted tokens" behavior is accepted in order to reduce the complexity of the contract as well as it's gas usage.
-        @dev this function is internal, use the increase/decreaseMintingAllowance functions instead. See https://www.adrianhetman.com/unboxing-erc20-approve-issues/
-        @param _minter address of the minter
-        @param _allowance maximum amount of tokens that can be minted by this minter (excluding the tokens minted as a fee)
-    */
-    function setMintingAllowance(
-        address _minter,
-        uint256 _allowance
-    ) internal onlyRole(MINTALLOWER_ROLE) {
-        mintingAllowance[_minter] = _allowance;
-        emit MintingAllowanceChanged(_minter, _allowance);
-    }
-
-    /** 
         @notice minting contracts such as personal investment invite, vesting, crowdfunding must be granted a minting allowance.
         @notice the contract does not keep track of how many tokens a minter has minted over time
         @param _minter address of the minter
