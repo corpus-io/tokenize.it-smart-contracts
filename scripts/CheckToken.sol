caf94ff60b59a9e5eac9846999fc17c424e16d79
malteish <malteish@corpus.io>
2023-01-18 12:41:24 +0100

    document and fix the CheckToken script

--- scripts/CheckToken.sol
+++ scripts/CheckToken.sol
@@ -7,15 +7,26 @@ import "../lib/forge-std/src/Script.sol";
 import "../contracts/FeeSettings.sol";
 import "../contracts/AllowList.sol";
 import "../contracts/PersonalInviteFactory.sol";
+import "../contracts/Token.sol";
 
-address constant TOKEN_ADDRESS = 0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1; // 0x9693f471524Ba0629e540f368573711C76ACBABa;
+
+/*
+    This script is used to check the deployed token contract.
+    It is not meant to be used in production, but rather as a reference for how to interact with the deployed contracts.
+
+    To run this script, use the following command:
+        forge script CheckToken.sol --rpc-url $GOERLI_RPC_URL
+
+*/
+
+address constant TOKEN_ADDRESS = 0x9693f471524Ba0629e540f368573711C76ACBABa;
 address constant TRUSTED_FORWARDER = 0x994257AcCF99E5995F011AB2A3025063e5367629;
 address constant FEE_SETTINGS = 0x147addF9C8E4030F8104c713Dad2A1d76E6c85a1;
 address constant ALLOW_LIST = 0x274ca5f21Cdde06B6E4Fe063f5087EB6Cf3eAe55;
 address constant ADMIN = 0x45a1cbD9788f5eA4061640ad7CB55031AE62b9dB;
 bytes32 constant DEFAULT_ADMIN_ROLE = 0x00;
 
-contract DeployPlatform is Script {
+contract CheckToken is Script {
     function run() public view {
         Token token = Token(address(TOKEN_ADDRESS));
 
