9281aa07278231cdee220350849eae5457ca2a65
Christoph Jentzsch <jentzsch.software@gmail.com>
2023-03-18 11:11:26 +0100

    add monerium accounts - add comment

--- contracts/MoneriumConstructorOnly.sol
+++ contracts/MoneriumConstructorOnly.sol
@@ -3,9 +3,20 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/*
+The contract is deployed by the fundraising company and is getting an IBAN by Monerium. By sending EUR to this IBAN, the investors agrees to the investment.
+The then minted EURe are approved for the corresponding investment contract. In case of an personal invite, it can be executed by the company.
+In ces of a continuous fundraising, the investor can now call the `buy`function in order to make an investment.
+This contract is inteneded to be used only once per investment per investor.
+*/
+
 contract MoneriumInterfacePersonalInvite {
     constructor(address _investment, uint256 _amount){
         IERC20(0x3231Cb76718CDeF2155FC47b5286d82e6eDA273f).approve(address(_investment), _amount);
     }
+
+    /* warning: In the case the investment contract does not work anymore (expired personal invites, paused fundraising, ...). All the EURe in this contract are techincally stuck.
+    But since this IBAN belongs to the company, they can make a IBAN transfer back to the investor using the Monerium WebApp. Monerium will then burn the tokens belonging to this address.
+    */
 }
 
