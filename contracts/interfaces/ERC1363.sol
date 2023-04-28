aae4eed70c792525b2ff85f08eac9d9b397c1c87
malteish <malteish@users.noreply.github.com>
2023-04-28 12:59:54 +0000

    Prettified Code!

--- contracts/interfaces/ERC1363.sol
+++ contracts/interfaces/ERC1363.sol
@@ -5,5 +5,10 @@ pragma solidity 0.8.17;
     @notice ERC1363 interface, see https://eips.ethereum.org/EIPS/eip-1363
  */
 interface ERC1363Receiver {
-  function onTransferReceived(address operator, address from, uint256 value, bytes memory data) external returns (bytes4);
+    function onTransferReceived(
+        address operator,
+        address from,
+        uint256 value,
+        bytes memory data
+    ) external returns (bytes4);
 }
