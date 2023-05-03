999bf3c5dd589681f652ccc05be2856a3f33625a
malteish <malteish@corpus.io>
2023-05-03 12:33:58 +0200

    test permit for EUROC

--- test/MainnetCurrenciesInvest.t.sol
+++ test/MainnetCurrenciesInvest.t.sol
@@ -17,7 +17,6 @@ import "./resources/ERC20Helper.sol";
 
 contract MainnetCurrencies is Test {
     using SafeERC20 for IERC20;
-    using stdStorage for StdStorage; // for stdStorage.set()
 
     ERC20Helper helper = new ERC20Helper();
 
@@ -55,12 +54,12 @@ contract MainnetCurrencies is Test {
     // generate address of invite
     bytes32 salt = bytes32(0);
 
-    // test currencies
-    IERC20 USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-    IERC20 EUROC = IERC20(0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c);
-    IERC20 DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    // // test currencies
+    // IERC20 USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    // IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    // IERC20 WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+    // IERC20 EUROC = IERC20(0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c);
+    // IERC20 DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
     function setUp() public {
         list = new AllowList();
