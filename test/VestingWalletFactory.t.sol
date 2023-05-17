72f5dbb7f92f10b3240bc926f680f3df4b4af532
malteish <malteish@corpus.io>
2023-05-17 15:05:15 +0200

    add test

--- test/VestingWalletFactory.t.sol
+++ test/VestingWalletFactory.t.sol
@@ -21,13 +21,11 @@ contract VestingWalletFactoryTest is Test {
         uint256 rawSalt,
         address beneficiaryAddress,
         uint64 startTimestamp,
-        uint64 durationSeconds,
-        uint256 amount
+        uint64 durationSeconds
     ) public {
         vm.assume(beneficiaryAddress != address(0));
         vm.assume(startTimestamp > block.timestamp);
         vm.assume(durationSeconds > 0);
-        vm.assume(amount > 0);
         //uint256 rawSalt = 0;
         bytes32 salt = bytes32(rawSalt);
 
@@ -40,10 +38,6 @@ contract VestingWalletFactoryTest is Test {
         }
         assertTrue(len == 0, "A contract has already been deployed at this address");
 
-        // take some risk: mint currency to vesting wallet before the contract is deployed
-        assertTrue(currency.balanceOf(expectedAddress) == 0, "currency.balanceOf(expectedAddress) != 0");
-        currency.mint(expectedAddress, amount);
-
         vm.expectEmit(true, true, true, true, address(factory));
         emit Deploy(expectedAddress);
         address actualAddress = factory.deploy(salt, beneficiaryAddress, startTimestamp, durationSeconds);
@@ -55,8 +49,47 @@ contract VestingWalletFactoryTest is Test {
             len := extcodesize(expectedAddress)
         }
         assertTrue(len != 0, "Contract deployment failed");
+    }
 
-        // make sure withdraw fails before startTimestamp
-        assert(currency.balanceOf(expectedAddress) == amount);
+    function testWalletVestsAsExpected(
+        uint256 rawSalt,
+        address beneficiaryAddress,
+        uint64 startTimestamp,
+        uint64 durationSeconds,
+        uint256 amount
+    ) public {
+        vm.assume(beneficiaryAddress != address(0));
+        vm.assume(startTimestamp > block.timestamp);
+        vm.assume(type(uint64).max - durationSeconds > startTimestamp);
+        vm.assume(amount < type(uint128).max);
+        vm.assume(amount > 0);
+        vm.assume(durationSeconds > 0);
+        //uint256 rawSalt = 0;
+        bytes32 salt = bytes32(rawSalt);
+
+        VestingWallet vestingWallet = VestingWallet(
+            payable(factory.deploy(salt, beneficiaryAddress, startTimestamp, durationSeconds))
+        );
+
+        // mint currency to vesting wallet
+        assertTrue(currency.balanceOf(address(vestingWallet)) == 0, "currency.balanceOf(expectedAddress) != 0");
+        currency.mint(address(vestingWallet), amount);
+        assertTrue(currency.balanceOf(address(vestingWallet)) == amount, "currency balance wrong");
+
+        // make sure nothing can be withdrawn before startTimestamp
+        assertTrue(vestingWallet.releasable(address(currency)) == 0, "tokens are releasable before startTimestamp");
+
+        // make sure nothing can be withdrawn at startTimestamp
+        vm.warp(startTimestamp);
+        assertTrue(vestingWallet.releasable(address(currency)) == 0, "tokens are releasable at startTimestamp");
+
+        // make sure everything can be withdrawn at startTimestamp + durationSeconds
+        vm.warp(startTimestamp + durationSeconds);
+        assertTrue(vestingWallet.releasable(address(currency)) == amount, "tokens are not releasable at end");
+
+        // withdraw and check amount
+        assertTrue(currency.balanceOf(beneficiaryAddress) == 0, "beneficiary already has currency");
+        vestingWallet.release(address(currency));
+        assertTrue(currency.balanceOf(beneficiaryAddress) == amount, "beneficiary has wrong amount of currency");
     }
 }
