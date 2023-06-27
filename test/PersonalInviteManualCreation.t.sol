8f6b18fe8270e454126769b6008b62451fae86e4
malteish <malteish@corpus.io>
2023-06-27 12:03:43 +0200

    test personal invite manually, update solc config

---
+++ test/PersonalInviteManualCreation.t.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Test.sol";
+import "../contracts/Token.sol";
+import "../contracts/PersonalInvite.sol";
+import "../contracts/PersonalInviteFactory.sol";
+import "../contracts/FeeSettings.sol";
+
+contract PersonalInviteManualCreationTest is Test {
+    event Deploy(address indexed addr);
+
+    AllowList list;
+    FeeSettings feeSettings;
+
+    Token token;
+    Token currency; // todo: add different ERC20 token as currency!
+
+    uint256 MAX_INT = type(uint256).max;
+
+    address public constant admin = 0x0109709eCFa91a80626FF3989D68f67f5b1dD120;
+    address public constant buyer = 0x1109709ecFA91a80626ff3989D68f67F5B1Dd121;
+    address public constant mintAllower = 0x2109709EcFa91a80626Ff3989d68F67F5B1Dd122;
+    address public constant minter = 0x3109709ECfA91A80626fF3989D68f67F5B1Dd123;
+    address public constant deployer = 0x6109709EcFA91A80626FF3989d68f67F5b1dd126;
+    address public constant receiver = 0x7109709eCfa91A80626Ff3989D68f67f5b1dD127;
+    address public constant paymentTokenProvider = 0x8109709ecfa91a80626fF3989d68f67F5B1dD128;
+    address public constant trustedForwarder = 0x9109709EcFA91A80626FF3989D68f67F5B1dD129;
+
+    uint256 public constant price = 10000000;
+
+    // function calcAddress(bytes memory bytecode, uint salt, address sender) public pure returns (address) {
+    //     bytes32 saltBytes = bytes32(salt);
+    //     bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), sender, saltBytes, keccak256(bytecode)));
+    //     return address(uint160(uint256(hash)));
+    // }
+
+    // function testCalcAddressWithExample() public view {
+    //     // example taken from https://docs.ethers.io/v5/api/utils/address/#utils-getCreate2Address
+    //     address sender = 0x8ba1f109551bD432803012645Ac136ddd64DBA72;
+    //     uint salt = 0x7c5ea36004851c764c44143b1dcb59679b11c9a68e5f41497f6cf3d480715331;
+    //     string memory hexString = "0x6394198df16000526103ff60206004601c335afa6040516060f3";
+    //     bytes memory bytecode = bytes(hexString);
+
+    //     address actual = calcAddress(bytecode, salt, sender);
+    //     address expected = 0x533ae9d683B10C02EbDb05471642F85230071FC3;
+
+    //     console.log("bytecode: %s", string(bytecode));
+
+    //     console.log("actual: %s", actual);
+    //     console.log("expected: %s", expected);
+    //     //assertEq(actual, expected);
+
+    //     // TODO: figure out why this fails
+
+    // }
+
+    function setUp() public {
+        list = new AllowList();
+        Fees memory fees = Fees(100, 100, 100, 0);
+        feeSettings = new FeeSettings(fees, admin);
+
+        token = new Token(trustedForwarder, feeSettings, admin, list, 0x0, "token", "TOK");
+        currency = new Token(trustedForwarder, feeSettings, admin, list, 0x0, "currency", "CUR");
+    }
+
+    function testDeployContract() public {
+        //bytes memory creationCode = type(PersonalInvite).creationCode;
+        uint256 amount = 20000000000000;
+        uint256 expiration = block.timestamp + 1000;
+
+        // nonce0= address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x80))))));
+        // nonce1= address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x01))))));
+
+        // contract will be deployed by deployer, which will have nonce 0
+
+        address expectedAddress = address(
+            uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), deployer, bytes1(0x80)))))
+        );
+        // address expectedAddress = factory.getAddress(
+        //     salt,
+        //     buyer,
+        //     buyer,
+        //     receiver,
+        //     amount,
+        //     price,
+        //     expiration,
+        //     currency,
+        //     token
+        // );
+
+        // make sure no contract lives here yet
+        uint256 len;
+        assembly {
+            len := extcodesize(expectedAddress)
+        }
+        assert(len == 0);
+
+        vm.prank(admin);
+        token.increaseMintingAllowance(expectedAddress, amount);
+
+        vm.prank(admin);
+        currency.increaseMintingAllowance(admin, amount * price);
+
+        vm.prank(admin);
+        currency.mint(buyer, amount * price);
+        vm.prank(buyer);
+        currency.approve(expectedAddress, amount * price);
+
+        uint256 gasBefore = gasleft();
+        vm.prank(deployer);
+        new PersonalInvite(buyer, buyer, receiver, amount, price, expiration, currency, token);
+        uint256 gasAfter = gasleft();
+        console.log("gas used: %s", gasBefore - gasAfter);
+
+        // make sure contract lives here now
+        assembly {
+            len := extcodesize(expectedAddress)
+        }
+        assert(len != 0);
+        console.log("contract bytecode size in bytes: %s", len);
+    }
+}
