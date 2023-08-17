a11e9ce0954d00f6c6c6c53b3a1b10fa6f4ee89f
malteish <malteish@corpus.io>
2023-08-17 11:18:09 +0200

    add upgradeable contracts and token clone factory

---
+++ contracts/CloneFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+abstract contract CloneFactory {
+    event NewClone(address clone);
+
+    /// The address of the implementation to clone
+    address immutable implementation;
+
+    constructor(address _implementation) {
+        require(_implementation != address(0), "CloneFactory: implementation can not be zero");
+        implementation = _implementation;
+    }
+
+    /**
+     * @notice Predicts the address of a clone that will be created
+     * @param salt The salt used to deterministically generate the clone address
+     * @return The address of the clone that will be created
+     * @dev This function does not check if the clone has already been created
+     */
+    function predictCloneAddress(bytes32 salt) public view returns (address) {
+        return Clones.predictDeterministicAddress(implementation, salt);
+    }
+}
