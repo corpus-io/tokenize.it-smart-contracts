53a944c653d0d9130756883868fc118653e9ca37
malteish <malteish@corpus.io>
2023-08-22 14:09:21 +0200

    update docs on contract verification

--- verificationArguments/hardhat/TokenGoerli.js
+++ verificationArguments/hardhat/TokenGoerli.js
@@ -1,9 +1,9 @@
 module.exports = [
   '0x0445d09A1917196E1DC12EdB7334C70c1FfB1623', // _trustedForwarder
   '0x387aD1Aa745C70829b651B3F2D3E7852Df961C93', // _feeSettings
-  '0x8530b74B2a143D07290AD2568aA2738b53820Fd5', // _admin
+  '0x2Db0DD9394f851baefD1FA3334c6B188A0C0548D', // _admin
   '0x274ca5f21Cdde06B6E4Fe063f5087EB6Cf3eAe55', // _allowList
   '0', // _requirements
-  'founder5', // _name
-  'FIVET', // _symbol
+  'Max Mustermann Token', // _name
+  'MAXMT', // _symbol
 ];
