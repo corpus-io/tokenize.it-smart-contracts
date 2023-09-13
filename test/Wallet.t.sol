3909ce366abb2b7a3368d21183e38ab0de6fcca6
malteish <malteish@corpus.io>
2023-09-13 10:18:59 +0200

    fix empty test

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -10,22 +10,19 @@ import "./resources/MaliciousPaymentToken.sol";
 import "../contracts/Wallet.sol";
 
 contract WalletTest is Test {
-   
-    function setUp() public {
-    }
+    function setUp() public {}
 
     function testSignAsContract(uint256 ownerPrivateKey, address fundraising, string memory message) public {
         vm.assume(ownerPrivateKey != 0);
-        address owner = vm.addressFromPrivateKey(ownerPrivateKey);
+        vm.assume(ownerPrivateKey < 2 ** 250 - 1);
+        address owner = vm.addr(ownerPrivateKey);
         vm.assume(fundraising != address(0));
         vm.assume(bytes(message).length > 0);
 
         bytes32 messageHash = keccak256(abi.encodePacked(message));
 
-        Wallet wallet = new Wallet(fundraising);
+        Wallet wallet = new Wallet(ContinuousFundraising(fundraising));
 
         // todo: create signature and check with contract
-        
     }
-
-    
+}
