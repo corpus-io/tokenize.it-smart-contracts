41728122c608b368775f4d5623d83b4263a6d921
malteish <malteish@corpus.io>
2023-11-03 11:28:09 +0100

    fix tests

--- test/MainnetCurrenciesInvest.t.sol
+++ test/MainnetCurrenciesInvest.t.sol
@@ -61,7 +61,7 @@ contract MainnetCurrencies is Test {
 
     function setUp() public {
         list = new AllowList();
-        Fees memory fees = Fees(100, 100, 100, 0);
+        Fees memory fees = Fees(1, 100, 1, 100, 1, 100, 0);
         feeSettings = new FeeSettings(fees, admin, admin, admin);
 
         Token implementation = new Token(trustedForwarder);
