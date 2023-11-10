3373660747aeda8f2f62f1f8ee50c7618fb47410
malteish <malteish@corpus.io>
2023-11-10 13:31:31 +0100

    update hardhat, ethers,  and others

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,9 @@ import { HardhatUserConfig, task } from 'hardhat/config';
 import 'dotenv/config';
 import 'hardhat-gas-reporter';
 import '@nomiclabs/hardhat-etherscan';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
 import '@typechain/hardhat';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
 
 // require("@nomiclabs/hardhat-waffle");
 // require("hardhat-gas-reporter");
@@ -78,13 +78,13 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  typechain: {
-    outDir: 'types',
-    target: 'ethers-v5',
-    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
-    dontOverrideCompile: false, // defaults to false
-  },
+  // typechain: {
+  //   outDir: 'types',
+  //   target: 'ethers-v6',
+  //   alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+  //   externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  //   dontOverrideCompile: false, // defaults to false
+  // },
 };
 
 export default config;
