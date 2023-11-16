5c17b104f73d7ad0b80d6f59d35833c33058da64
Christoph Jentzsch <jentzsch.software@gmail.com>
2023-11-16 13:55:16 +0100

    simplify

--- contracts/PriceLinearCloneFactory.sol
+++ contracts/PriceLinearCloneFactory.sol
@@ -61,33 +61,7 @@ contract PriceLinearCloneFactory is CloneFactory {
         bool _isBlockBased,
         bool _isRising
     ) external view returns (address) {
-        bytes32 salt = _generateSalt(
-            _rawSalt,
-            _trustedForwarder,
-            _owner,
-            _slopeEnumerator,
-            _slopeDenominator,
-            _startTimeOrBlockNumber,
-            _stepDuration,
-            _isBlockBased,
-            _isRising
-        );
-        return Clones.predictDeterministicAddress(implementation, salt);
-    }
-
-    function _generateSalt(
-        bytes32 _rawSalt,
-        address _trustedForwarder,
-        address _owner,
-        uint64 _slopeEnumerator,
-        uint64 _slopeDenominator,
-        uint64 _startTimeOrBlockNumber,
-        uint32 _stepDuration,
-        bool _isBlockBased,
-        bool _isRising
-    ) internal pure returns (bytes32) {
-        return
-            keccak256(
+        bytes32 salt = keccak256(
                 abi.encodePacked(
                     _rawSalt,
                     _trustedForwarder,
@@ -99,6 +73,6 @@ contract PriceLinearCloneFactory is CloneFactory {
                     _isBlockBased,
                     _isRising
                 )
-            );
+        );
+        return Clones.predictDeterministicAddress(implementation, salt);
     }
-}
