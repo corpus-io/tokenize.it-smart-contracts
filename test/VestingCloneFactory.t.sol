065e14de461fe3041d8c7eed1c997b5c5d4877b6
malteish <malteish@corpus.io>
2023-11-17 16:30:49 +0100

    test trustedForwarder in VestingCloneFactory

--- test/VestingCloneFactory.t.sol
+++ test/VestingCloneFactory.t.sol
@@ -5,6 +5,7 @@ import "../lib/forge-std/src/Test.sol";
 import "../contracts/factories/VestingCloneFactory.sol";
 
 contract VestingCloneFactoryTest is Test {
+    event NewClone(address clone);
     Vesting implementation;
     VestingCloneFactory factory;
 
@@ -28,8 +29,26 @@ contract VestingCloneFactoryTest is Test {
         address expected2 = _factory.predictCloneAddress(_salt, _trustedForwarder, _owner, _token);
         assertEq(expected1, expected2, "address prediction with salt and params not equal");
 
+        vm.expectEmit(true, true, true, true, address(_factory));
+        emit NewClone(expected1);
         address actual = _factory.createVestingClone(_salt, _trustedForwarder, _owner, _token);
         assertEq(expected1, actual, "address prediction failed");
+    }
+
+    function testWrongTrustedForwarderReverts(address _wrongTrustedForwarder) public {
+        vm.assume(_wrongTrustedForwarder != address(0));
+        vm.assume(_wrongTrustedForwarder != trustedForwarder);
+
+        bytes32 _salt = keccak256(abi.encodePacked(_wrongTrustedForwarder));
+        address _owner = address(2);
+        address _token = address(3);
+
+        // test wrong trustedForwarder reverts
+        vm.expectRevert("VestingCloneFactory: Unexpected trustedForwarder");
+        factory.predictCloneAddress(_salt, _wrongTrustedForwarder, _owner, _token);
+
+        vm.expectRevert("VestingCloneFactory: Unexpected trustedForwarder");
+        factory.createVestingClone(_salt, _wrongTrustedForwarder, _owner, _token);
     }
 
     function testSecondDeploymentFails(bytes32 _salt, address _owner, address _token) public {
