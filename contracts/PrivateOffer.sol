9b68d9beac9d0ddab74d99108f1be149d8c966e3
malteish <malteish@corpus.io>
2023-12-04 08:46:15 +0100

    remove unused imports

--- contracts/PrivateOffer.sol
+++ contracts/PrivateOffer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./Token.sol";
