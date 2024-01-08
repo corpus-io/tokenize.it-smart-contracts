f0e2c35757b3c5f9d504fe3d74442b622830581d
malteish <malteish@corpus.io>
2024-01-08 08:32:18 +0100

    add sepolia definition

--- script/hardhatVerifyConstructorArguments/ContinuousFundraising.js
+++ script/hardhatVerifyConstructorArguments/ContinuousFundraising.js
@@ -1,10 +1,10 @@
 module.exports = [
-  '0x0445d09A1917196E1DC12EdB7334C70c1FfB1623', // _trustedForwarder
-  '0xA1e28D1f17b7Da62d10fbFaFCA98Fa406D759ce2', // _currencyReceiver
-  '10000000000000000000', // _minAmountPerBuyer
-  '50000000000000000000', // _maxAmountPerBuyer
-  '1000000', // _tokenPrice
-  '100000000000000000000', // _maxAmountOfTokenToBeSold
-  '0x07865c6E87B9F70255377e024ace6630C1Eaa37F', // _currency
-  '0x1672E16ac9CeF8f9Fc31daFB21d637a23415DEf6', // _token
+  '0x274ca5f21Cdde06B6E4Fe063f5087EB6Cf3eAe55', // _trustedForwarder
+  '0xccd4c4cecc265c8eb901455b1444d248adfaeaa9', // _currencyReceiver
+  '004500045000450004', // _minAmountPerBuyer
+  '444376444444444680000', // _maxAmountPerBuyer
+  '22222000000', // _tokenPrice
+  '444376444444444680000', // _maxAmountOfTokenToBeSold
+  '0x6D2c1a7EaC3783aF1Ff72cF074477581D04fD8EF', // _currency
+  '0x4418BC0Af0b55384b9d307227d9840a93E6253D7', // _token
 ];
