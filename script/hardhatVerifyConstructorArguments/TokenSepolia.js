bc31e3425bb1b6788a6d38b456fa78fc178bf5db
malteish <malteish@corpus.io>
2024-02-13 16:50:04 +0100

    add sepolia verification arguments

---
+++ script/hardhatVerifyConstructorArguments/TokenSepolia.js
@@ -0,0 +1,9 @@
+module.exports = [
+  '0x274ca5f21Cdde06B6E4Fe063f5087EB6Cf3eAe55', // _trustedForwarder
+  '0x47EE5950B9a790A292B731789a35CcCB7381667E', // _feeSettings
+  '0xa1e28d1f17b7da62d10fbfafca98fa406d759ce2', // _admin
+  '0x4b9Cd96B7aea5Ef9C334B7454938913B655b9269', // _allowList
+  '0', // _requirements
+  'Patrik Token V420', // _name
+  'TV420', // _symbol
+];
